99604578de28d2344312076c9f02512495231af0
felfeng <felfeng@ucdavis.edu>
2024-11-07 12:28:42 -0800

    initial changes

--- foodie-haven/src/App.vue
+++ foodie-haven/src/App.vue
@@ -40,7 +40,7 @@ export default {
   align-items: center;
   justify-content: space-between;
   padding: 10px 20px;
-  background-color: #2c3e50;
+  background-color: #003865;
   color: #fff;
 }
 
@@ -68,8 +68,8 @@ export default {
 .add-restaurant-btn {
   margin-right: 20px;
   padding: 8px 16px;
-  background-color: #4CAF50;
-  color: white;
+  background-color: white;
+  color: #003865;
   border: none;
   border-radius: 4px;
   cursor: pointer;
@@ -77,7 +77,7 @@ export default {
 }
 
 .add-restaurant-btn:hover {
-  background-color: #45a049;
+  background-color: #bdd6e6;
 }
 
 .profile-btn {
